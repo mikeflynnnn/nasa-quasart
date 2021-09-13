d0db66230f9fb2b1fbd8aac062e5b12ddc413ce3
Mike Flynn <mdflynn34@outlook.com>
2021-09-13 17:12:16 -0600

    export fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
-const fetchAPOD = async () => {
+export const fetchAPOD = async () => {
   // random 10 pics
   const url = "https://api.nasa.gov/planetary/apod?count=10&api_key=";
   const API_KEY = process.env.REACT_APP_NASA_API_KEY;
