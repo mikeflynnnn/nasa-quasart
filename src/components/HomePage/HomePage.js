463fae3657ed4d36583165d8ea0d2ede704ef6d9
Mike Flynn <mdflynn34@outlook.com>
2021-09-13 17:14:43 -0600

    import fetch and setState with data

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./HomePage.scss";
+
+import { fetchAPOD } from "../../apiCalls";
 
 const HomePage = () => {
+  const [randomPictures, setRandomPictures] = useState([]);
+
+  useEffect(() => {
+    fetchAPOD().then((data) => setRandomPictures(data));
+  });
+
   return <p>HomePage</p>;
 };
 
