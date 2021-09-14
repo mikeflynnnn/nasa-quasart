cf53ffc635ccc5c103ded2e616193878954001d0
Mike Flynn <mdflynn34@outlook.com>
2021-09-13 19:51:07 -0600

    pass likeAPicture as prop

--- src/components/PictureCard/PictureCard.js
+++ src/components/PictureCard/PictureCard.js
@@ -3,16 +3,16 @@ import "./PictureCard.scss";
 
 import LikeButton from "../LikeButton/Likebutton";
 
-const PictureCard = ({ pictureDetails }) => {
-  const { url, title, date, id } = pictureDetails;
+const PictureCard = ({ pictureDetails, like }) => {
+  const { url, title, date, id, liked } = pictureDetails;
 
   return (
     <article className="details-container">
       <img className="details-img" src={url} alt={title} />
       <p className="details-title">{title}</p>
       <p className="details-date">{date}</p>
-      <div onClick={() => console.log('yo')}>
-        <LikeButton />
+      <div onClick={() => like(id)}>
+        <LikeButton liked={liked} />
       </div>
     </article>
   );
