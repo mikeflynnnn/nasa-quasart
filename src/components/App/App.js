580360cb0dc23dcb6dbd6d50034a4acf5ccd0aad
Mike Flynn <mdflynn34@outlook.com>
2021-09-15 17:52:49 -0600

    add switch, route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,11 +3,15 @@ import React from "react";
 import HomePage from "../HomePage/HomePage";
 import Nav from "../Nav/Nav";
 
+import { Switch, Route } from "react-router-dom";
+
 const App = () => {
   return (
     <>
       <Nav />
-      <HomePage />
+      <Switch>
+        <HomePage />
+      </Switch>
     </>
   );
 };
