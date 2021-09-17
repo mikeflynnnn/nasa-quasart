438ce4be455966dd40c0b0b0f279d2d81a2301a6
Mike Flynn <mdflynn34@outlook.com>
2021-09-17 16:03:34 -0600

    add explanation to picturecard

--- src/components/PictureCard/PictureCard.js
+++ src/components/PictureCard/PictureCard.js
@@ -4,13 +4,14 @@ import "./PictureCard.scss";
 import LikeButton from "../LikeButton/Likebutton";
 
 const PictureCard = ({ pictureDetails, like }) => {
-  const { url, title, date, id, liked } = pictureDetails;
+  const { url, title, date, id, liked, explanation } = pictureDetails;
 
   return (
     <article className="details-container">
       <img className="details-img" src={url} alt={title} />
       <p className="details-title">{title}</p>
       <p className="details-date">{date}</p>
+      <p>{explanation}</p>
       <div onClick={() => like(id)}>
         <LikeButton liked={liked} />
       </div>
