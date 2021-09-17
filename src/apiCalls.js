648d25ba5ca7534d9cc2886ac6e67dc6cce3f611
Mike Flynn <mdflynn34@outlook.com>
2021-09-17 16:35:06 -0600

    update to 20 images

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 export const fetchAPOD = async () => {
   // random 10 pics
-  const url = "https://api.nasa.gov/planetary/apod?count=10&api_key=";
+  const url = "https://api.nasa.gov/planetary/apod?count=20&api_key=";
   const API_KEY = process.env.REACT_APP_NASA_API_KEY;
 
   const response = await fetch(url + API_KEY);
