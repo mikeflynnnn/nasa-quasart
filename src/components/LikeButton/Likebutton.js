5f36cbc40e6c273e99a8876fc8bad7d42093e05e
Mike Flynn <mdflynn34@outlook.com>
2021-09-21 11:44:31 -0600

    import heart imgs

--- src/components/LikeButton/Likebutton.js
+++ src/components/LikeButton/Likebutton.js
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Likebutton.scss";
 
+import blueHeart from "../../assets/blue-heart.svg"
+import redHeart from "../../assets/red-heart.svg"
+
 const LikeButton = ({ liked }) => {
   return (
-    <button className={`like-button ${liked ? "favorited" : ""}`}>Like</button>
+    <button className={`like-button ${liked ? "favorited" : ""}`}>
+      <img src={redHeart} alt="heart button"/>
+    </button>
   );
 };
 
