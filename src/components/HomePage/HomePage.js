1f67127b9479b66b7273027fe5902625697f8353
Mike Flynn <mdflynn34@outlook.com>
2021-09-21 13:15:53 -0600

    add no favorite text

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./HomePage.scss";
 
 const HomePage = ({ pictures }) => {
-  return <section className="picture-display">{pictures}</section>;
+  
+  const noFavoriteImages =
+    pictures.length > 0 ? pictures : "Add some pictures to your favorites!";
+
+  return <section className="picture-display">{noFavoriteImages}</section>;
 };
 
 export default HomePage;
