0c4d1ac45302791c1ecfeab9d6e96e3bad1d8311
Mike Flynn <mdflynn34@outlook.com>
2021-09-21 10:31:33 -0600

    add formatDate function. Update classes

--- src/components/PictureCard/PictureCard.js
+++ src/components/PictureCard/PictureCard.js
@@ -6,14 +6,45 @@ import LikeButton from "../LikeButton/Likebutton";
 const PictureCard = ({ pictureDetails, like }) => {
   const { url, title, date, id, liked, explanation } = pictureDetails;
 
+  // add to utility file
+  const months = {
+    "01": "January",
+    "02": "February",
+    "03": "March",
+    "04": "April",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "August",
+    "09": "September",
+    10: "October",
+    11: "November",
+    12: "December",
+  };
+
+  const formatDate = (dateToFormat) => {
+    const splitDate = dateToFormat.split("-");
+    const updatedDate = `${months[splitDate[1]]} ${splitDate[2]}, ${
+      splitDate[0]
+    }`;
+
+    return updatedDate;
+  };
+
+  const formatedDate = formatDate(date);
+
   return (
-    <article className="details-container">
-      <img className="details-img" src={url} alt={title} />
-      <p className="details-title">{title}</p>
-      <p className="details-date">{date}</p>
-      <p>{explanation}</p>
-      <div onClick={() => like(id)}>
-        <LikeButton liked={liked} />
+    <article className="apod-container">
+      <img className="apod-img" src={url} alt={title} />
+      <div className="apod-details">
+        <div className="apod-title-container">
+          <p className="apod-title">{title}</p>
+          <p className="apod-date">- {formatedDate}</p>
+        </div>
+        <p>{explanation}</p>
+        <div onClick={() => like(id)}>
+          <LikeButton liked={liked} />
+        </div>
       </div>
     </article>
   );
