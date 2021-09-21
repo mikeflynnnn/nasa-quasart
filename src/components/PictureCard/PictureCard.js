74e60ed6937e67e139b673eff7ef83ee21a0d1c2
Mike Flynn <mdflynn34@outlook.com>
2021-09-21 14:48:16 -0600

    move func to utilities

--- src/components/PictureCard/PictureCard.js
+++ src/components/PictureCard/PictureCard.js
@@ -2,34 +2,10 @@ import React from "react";
 import "./PictureCard.scss";
 
 import LikeButton from "../LikeButton/Likebutton";
+import { formatDate } from "../../utilities";
 
 const PictureCard = ({ pictureDetails, like }) => {
   const { url, title, date, id, liked, explanation } = pictureDetails;
-
-  // add to utility file
-  const months = {
-    "01": "January",
-    "02": "February",
-    "03": "March",
-    "04": "April",
-    "05": "May",
-    "06": "June",
-    "07": "July",
-    "08": "August",
-    "09": "September",
-    10: "October",
-    11: "November",
-    12: "December",
-  };
-
-  const formatDate = (dateToFormat) => {
-    const splitDate = dateToFormat.split("-");
-    const updatedDate = `${months[splitDate[1]]} ${splitDate[2]}, ${
-      splitDate[0]
-    }`;
-
-    return updatedDate;
-  };
 
   const formatedDate = formatDate(date);
 
